1b14539b0a3221916e6efe7c12d60e190d27e825
Szabolcs Vasas <vasas@cloudera.com>
2017-08-24 14:55:19 +0200

    Experimenting with ant custom tasks

---
+++ src/test/org/apache/sqoop/tool/ant/FindTestsByCategoriesTask.java
@@ -0,0 +1,49 @@
+package org.apache.sqoop.tool.ant;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+
+/*
+Desired output:
+"org/apache/sqoop/tool/TestBaseSqoopTool.java,org/apache/sqoop/orm/TestCompilationManager.java,org/apache/sqoop/TestSqoopOptions.java"
+ */
+
+public class FindTestsByCategoriesTask extends Task {
+
+  private static final String TEST_PATTERN_PROPERTY = "test.pattern";
+
+  private static final String TEST_CATEGORY_SEPARATOR = ",";
+
+  private Collection<Class<?>> testCategories;
+
+  public FindTestsByCategoriesTask() {
+    testCategories = new ArrayList<>();
+  }
+
+  @Override
+  public void execute() throws BuildException {
+
+    System.out.println("Found these classes: " + testCategories);
+
+  }
+
+  public void setTestCategories(String testCategoriesString) {
+    try {
+      for (String testCategoryString : splitTestCategoriesString(testCategoriesString)) {
+        testCategories.add(Class.forName(testCategoryString));
+      }
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Collection<String> splitTestCategoriesString(String testCategoriesString) {
+    return Arrays.asList(testCategoriesString.split(TEST_CATEGORY_SEPARATOR));
+  }
+
+}
