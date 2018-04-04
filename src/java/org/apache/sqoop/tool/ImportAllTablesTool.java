b61a83b39725e706674edc987d4289e604f9ca16
Szabolcs Vasas <vasas@apache.org>
2018-04-04 10:44:03 +0200

    Reference to HiveImport is removed from ImportAllTablesTool.

--- src/java/org/apache/sqoop/tool/ImportAllTablesTool.java
+++ src/java/org/apache/sqoop/tool/ImportAllTablesTool.java
@@ -31,7 +31,6 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.sqoop.SqoopOptions;
 import org.apache.sqoop.SqoopOptions.InvalidOptionsException;
 import org.apache.sqoop.cli.RelatedOptions;
-import org.apache.sqoop.hive.HiveImport;
 import org.apache.sqoop.util.ImportException;
 
 /**
@@ -75,7 +74,6 @@ public class ImportAllTablesTool extends ImportTool {
   @Override
   /** {@inheritDoc} */
   public int run(SqoopOptions options) {
-    HiveImport hiveImport = null;
     Set<String> excludes = new HashSet<String>();
 
     if (!init(options)) {
