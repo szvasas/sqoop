2488de0f8a635ce97379964064e91c7f1ffa1636
Szabolcs Vasas <vasas@apache.org>
2018-06-12 14:23:40 +0200

    TestHiveServer2ParquetImport now uses Enclosed.

--- src/test/org/apache/sqoop/hive/TestHiveServer2ParquetImport.java
+++ src/test/org/apache/sqoop/hive/TestHiveServer2ParquetImport.java
@@ -24,19 +24,18 @@ import org.apache.sqoop.hive.minicluster.NoAuthenticationConfiguration;
 import org.apache.sqoop.testutil.ArgumentArrayBuilder;
 import org.apache.sqoop.testutil.HiveServer2TestUtil;
 import org.apache.sqoop.testutil.ImportJobTestCase;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -46,13 +45,8 @@ import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-@RunWith(Parameterized.class)
-public class TestHiveServer2ParquetImport extends ImportJobTestCase {
-
-  @Parameters(name = "compressionCodec = {0}")
-  public static Iterable<? extends Object> authenticationParameters() {
-    return Arrays.asList("snappy", "gzip");
-  }
+@RunWith(Enclosed.class)
+public class TestHiveServer2ParquetImport {
 
   private static final String[] TEST_COLUMN_TYPES = {"VARCHAR(32)", "INTEGER", "CHAR(64)"};
 
@@ -60,143 +54,171 @@ public class TestHiveServer2ParquetImport extends ImportJobTestCase {
 
   private static final List<Object> TEST_COLUMN_VALUES_LINE2 = Arrays.<Object>asList("test2", 4242, "somestring2");
 
-  @Rule
-  public ExpectedException expectedException = ExpectedException.none();
-
-  private final String compressionCodec;
-
   private static HiveMiniCluster hiveMiniCluster;
 
   private static HiveServer2TestUtil hiveServer2TestUtil;
 
-  public TestHiveServer2ParquetImport(String compressionCodec) {
-    this.compressionCodec = compressionCodec;
-  }
+  @RunWith(Parameterized.class)
+  public static class ParquetCompressionCodecTestCase extends ImportJobTestCase {
 
-  @BeforeClass
-  public static void beforeClass() {
-    hiveMiniCluster = new HiveMiniCluster(new NoAuthenticationConfiguration());
-    hiveMiniCluster.start();
-    hiveServer2TestUtil = new HiveServer2TestUtil(hiveMiniCluster.getUrl());
-  }
-
-  @AfterClass
-  public static void afterClass() {
-    hiveMiniCluster.stop();
-  }
-
-  @Override
-  @Before
-  public void setUp() {
-    super.setUp();
-
-    createTableWithColTypes(TEST_COLUMN_TYPES, toStringArray(TEST_COLUMN_VALUES));
-  }
-
-  @Override
-  @After
-  public void tearDown() {
-    try {
-      dropTableIfExists(getTableName());
-    } catch (SQLException e) {
-      throw new RuntimeException();
+    @Parameters(name = "compressionCodec = {0}")
+    public static Iterable<? extends Object> authenticationParameters() {
+      return Arrays.asList("snappy", "gzip");
     }
 
-    super.tearDown();
+    @BeforeClass
+    public static void beforeClass() {
+      TestHiveServer2ParquetImport.beforeClass();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+      TestHiveServer2ParquetImport.afterClass();
+    }
+
+    private final String compressionCodec;
+
+    public ParquetCompressionCodecTestCase(String compressionCodec) {
+      this.compressionCodec = compressionCodec;
+    }
+
+    @Override
+    @Before
+    public void setUp() {
+      super.setUp();
+
+      createTableWithColTypes(TEST_COLUMN_TYPES, toStringArray(TEST_COLUMN_VALUES));
+    }
+
+    @Test
+    public void testHiveImportAsParquetWithCompressionCodec() throws Exception {
+      String[] args = commonArgs(getConnectString(), getTableName())
+          .withOption("compression-codec", compressionCodec)
+          .build();
+
+      runImport(args);
+
+      List<List<Object>> rows = hiveServer2TestUtil.loadRawRowsFromTable(getTableName());
+      assertThat(rows, hasItems(TEST_COLUMN_VALUES));
+    }
   }
 
-  @Test
-  public void testNormalHiveImportAsParquet() throws Exception {
-    String[] args = commonArgs().build();
+  public static class GeneralParquetTestCase extends ImportJobTestCase {
 
-    runImport(args);
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
-    List<List<Object>> rows = hiveServer2TestUtil.loadRawRowsFromTable(getTableName());
-    assertThat(rows, hasItems(TEST_COLUMN_VALUES));
+    @BeforeClass
+    public static void beforeClass() {
+      TestHiveServer2ParquetImport.beforeClass();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+      TestHiveServer2ParquetImport.afterClass();
+    }
+
+    @Override
+    @Before
+    public void setUp() {
+      super.setUp();
+
+      createTableWithColTypes(TEST_COLUMN_TYPES, toStringArray(TEST_COLUMN_VALUES));
+    }
+
+    @Test
+    public void testNormalHiveImportAsParquet() throws Exception {
+      String[] args = commonArgs(getConnectString(), getTableName()).build();
+
+      runImport(args);
+
+      List<List<Object>> rows = hiveServer2TestUtil.loadRawRowsFromTable(getTableName());
+      assertThat(rows, hasItems(TEST_COLUMN_VALUES));
+    }
+
+    @Test
+    public void testAppendHiveImportAsParquet() throws Exception {
+      String[] args = commonArgs(getConnectString(), getTableName()).build();
+
+      runImport(args);
+
+      insertIntoTable(TEST_COLUMN_TYPES, toStringArray(TEST_COLUMN_VALUES_LINE2));
+
+      runImport(args);
+
+      List<List<Object>> rows = hiveServer2TestUtil.loadRawRowsFromTable(getTableName());
+      assertThat(rows, hasItems(TEST_COLUMN_VALUES, TEST_COLUMN_VALUES_LINE2));
+    }
+
+    @Test
+    public void testCreateOverwriteHiveImportAsParquet() throws Exception {
+      String[] args = commonArgs(getConnectString(), getTableName())
+          .withOption("hive-overwrite")
+          .build();
+
+      runImport(args);
+
+      dropTableIfExists(getTableName());
+      createTableWithColTypes(TEST_COLUMN_TYPES, toStringArray(TEST_COLUMN_VALUES_LINE2));
+
+      runImport(args);
+
+      List<List<Object>> rows = hiveServer2TestUtil.loadRawRowsFromTable(getTableName());
+      assertEquals(asList(TEST_COLUMN_VALUES_LINE2), rows);
+    }
+
+    @Test
+    // TODO: this gives a bit different error message than the Kite one, it should be fixed.
+    public void testCreateHiveImportAsParquet() throws Exception {
+      String[] args = commonArgs(getConnectString(), getTableName())
+          .withOption("create-hive-table")
+          .build();
+
+      runImport(args);
+
+      expectedException.expect(IOException.class);
+      runImport(args);
+    }
+
+    @Test
+    public void testHiveImportAsParquetWhenTableExistsWithIncompatibleSchema() throws Exception {
+      String hiveTableName = "hiveImportAsParquetWhenTableExistsWithIncompatibleSchema";
+      String[] incompatibleSchemaTableTypes = {"INTEGER", "INTEGER", "INTEGER"};
+      List<Object> incompatibleSchemaTableData = Arrays.<Object>asList(100, 200, 300);
+
+      String[] args = commonArgs(getConnectString(), getTableName())
+          .withOption("hive-table", hiveTableName)
+          .build();
+
+      runImport(args);
+
+      // We make sure we create a new table in the test RDBMS.
+      incrementTableNum();
+      createTableWithColTypes(incompatibleSchemaTableTypes, toStringArray(incompatibleSchemaTableData));
+
+      // Recreate the argument array to pick up the new RDBMS table name.
+      args = commonArgs(getConnectString(), getTableName())
+          .withOption("hive-table", hiveTableName)
+          .build();
+
+      runImport(args);
+    }
+
   }
 
-  @Test
-  public void testAppendHiveImportAsParquet() throws Exception {
-    String[] args = commonArgs().build();
-
-    runImport(args);
-
-    insertIntoTable(TEST_COLUMN_TYPES, toStringArray(TEST_COLUMN_VALUES_LINE2));
-
-    runImport(args);
-
-    List<List<Object>> rows = hiveServer2TestUtil.loadRawRowsFromTable(getTableName());
-    assertThat(rows, hasItems(TEST_COLUMN_VALUES, TEST_COLUMN_VALUES_LINE2));
-  }
-
-  @Test
-  public void testCreateOverwriteHiveImportAsParquet() throws Exception {
-    String[] args = commonArgs()
-        .withOption("hive-overwrite")
-        .build();
-
-    runImport(args);
-
-    dropTableIfExists(getTableName());
-    createTableWithColTypes(TEST_COLUMN_TYPES, toStringArray(TEST_COLUMN_VALUES_LINE2));
-
-    runImport(args);
-
-    List<List<Object>> rows = hiveServer2TestUtil.loadRawRowsFromTable(getTableName());
-    assertEquals(asList(TEST_COLUMN_VALUES_LINE2), rows);
-  }
-
-  @Test
-  // TODO: this gives a bit different error message than the Kite one, it should be fixed.
-  public void testCreateHiveImportAsParquet() throws Exception {
-    String[] args = commonArgs()
-        .withOption("create-hive-table")
-        .build();
-
-    runImport(args);
-
-    expectedException.expect(IOException.class);
-    runImport(args);
-  }
-
-  @Test
-  public void testHiveImportAsParquetWhenTableExistsWithIncompatibleSchema() throws Exception {
-    String hiveTableName = "hiveImportAsParquetWhenTableExistsWithIncompatibleSchema";
-    String[] incompatibleSchemaTableTypes = {"INTEGER", "INTEGER", "INTEGER"};
-    List<Object> incompatibleSchemaTableData = Arrays.<Object>asList(100, 200, 300);
-
-    String[] args = commonArgs()
-        .withOption("hive-table", hiveTableName)
-        .build();
-
-    runImport(args);
-
-    // We make sure we create a new table in the test RDBMS.
-    incrementTableNum();
-    createTableWithColTypes(incompatibleSchemaTableTypes, toStringArray(incompatibleSchemaTableData));
-
-    // Recreate the argument array to pick up the new RDBMS table name.
-    args = commonArgs()
-        .withOption("hive-table", hiveTableName)
-        .build();
-
-    runImport(args);
-  }
-
-  private ArgumentArrayBuilder commonArgs() {
+  private static ArgumentArrayBuilder commonArgs(String connectString, String tableName) {
     return new ArgumentArrayBuilder()
         .withProperty(PARQUET_JOB_CONFIGURATOR_IMPLEMENTATION_KEY, "hadoop")
-        .withOption("connect", getConnectString())
-        .withOption("table", getTableName())
+        .withOption("connect", connectString)
+        .withOption("table", tableName)
         .withOption("hive-import")
         .withOption("hs2-url", hiveMiniCluster.getUrl())
-        .withOption("split-by", getColName(1))
+        .withOption("num-mappers", "1")
         .withOption("as-parquetfile")
-        .withOption("delete-target-dir")
-        .withOption("compression-codec", compressionCodec);
+        .withOption("delete-target-dir");
   }
 
-  private String[] toStringArray(List<Object> columnValues) {
+  private static String[] toStringArray(List<Object> columnValues) {
     String[] result = new String[columnValues.size()];
 
     for (int i = 0; i < columnValues.size(); i++) {
@@ -208,6 +230,16 @@ public class TestHiveServer2ParquetImport extends ImportJobTestCase {
     }
 
     return result;
+  }
+
+  public static void beforeClass() {
+    hiveMiniCluster = new HiveMiniCluster(new NoAuthenticationConfiguration());
+    hiveMiniCluster.start();
+    hiveServer2TestUtil = new HiveServer2TestUtil(hiveMiniCluster.getUrl());
+  }
+
+  public static void afterClass() {
+    hiveMiniCluster.stop();
   }
 
 }
