743d25e7edc89d2eade3d895edbc4fce8652c052
Szabolcs Vasas <vasas@apache.org>
2018-07-16 12:09:00 +0200

    Unused import is removed from TableDefWriter.

--- src/java/org/apache/sqoop/hive/TableDefWriter.java
+++ src/java/org/apache/sqoop/hive/TableDefWriter.java
@@ -35,7 +35,6 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.sqoop.avro.AvroUtil;
 import org.apache.sqoop.io.CodecMap;
