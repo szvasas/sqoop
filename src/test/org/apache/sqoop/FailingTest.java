8670ae776d12ffcbd65002a74d2025492e97f219
Szabolcs Vasas <vasas@apache.org>
2018-11-27 10:28:30 +0100

    FailingTest is added.

---
+++ src/test/org/apache/sqoop/FailingTest.java
@@ -0,0 +1,15 @@
+package org.apache.sqoop;
+
+import org.apache.sqoop.testcategories.sqooptest.UnitTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(UnitTest.class)
+public class FailingTest {
+
+  @Test
+  public void fail() {
+
+  }
+
+}
